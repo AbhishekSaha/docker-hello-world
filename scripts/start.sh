b8a410ad7bc56e01a58b808cbb995967ec1aef44
Abhishek Saha <as1695@scarletmail.rutgers.edu>
2024-08-26 17:53:22 -0700

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,5 +2,6 @@
 
 cd /var/hello-world
 sudo docker system prune -a -f
+make
 docker build --platform=linux/arm64 -t docker-hello-world:latest .
 docker run --platform=linux/arm64 -p 8080:8080 -d docker-hello-world:latest
