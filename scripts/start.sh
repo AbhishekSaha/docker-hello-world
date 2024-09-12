86d590123538c0653c980d03241fd540eddc1a4b
Abhishek Saha <as1695@scarletmail.rutgers.edu>
2024-09-11 23:37:35 -0700

    Added scripts back

---
+++ scripts/start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+cd /var/hello-world
+sudo docker system prune -a -f
+sudo systemctl start ecs
+sudo systemctl status ecs
+docker build --platform=linux/arm64 -t docker-hello-world:latest .
+docker run --platform=linux/arm64 -p 8080:8080 -d docker-hello-world:latest
